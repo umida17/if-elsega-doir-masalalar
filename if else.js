5faef4a27486abfdfd96e1f35679d0fe7b5891fc
uy <a.normurzaev20@gmail.com>
2026-03-27 23:33:34 +0500

    7 masala yakunlandi

--- if else.js
+++ if else.js
@@ -108,4 +108,33 @@
 
 
 
+
+// let d = +prompt(" 1 dan 7 gacha bolgan hafta kunini kiriting");
+
+// switch (d) {
+//     case 1:
+//         console.log("Dushanba");
+//         break;
+//      case 2:
+//         console.log("Seshanba");
+//         break;
+//      case 3:
+//         console.log("Chorshanba");
+//         break;
+//      case 4:
+//         console.log("Payshanba");
+//         break;
+//      case 5:
+//         console.log("Juma");
+//         break;
+//      case 6:
+//         console.log("Shanba");
+//         break;
+//      case 7:
+//         console.log("Yakshanba");
+//         break;
+//     default:
+//        console.log("Notog'ri son kiritildi ❌");
+       
+// }
  
