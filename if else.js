bdd65c27e1cdad56de3901ffca91d96c39aad24c
uy <a.normurzaev20@gmail.com>
2026-03-27 23:01:27 +0500

    2 masala ishlandi

--- if else.js
+++ if else.js
@@ -11,3 +11,24 @@
 // console.log(a);
 
 
+// let a = parseInt(prompt("1-sonni kiriting:"));
+// let b = parseInt(prompt("2-sonni kiriting:"));
+// let c = parseInt(prompt("3-sonni kiriting:"));
+
+// let musbat = 0;
+// let manfiy = 0;
+
+// // a soni 
+// if (a > 0) musbat++;
+// else if (a < 0) manfiy++;
+
+// // b ni 
+// if (b > 0) musbat++;
+// else if (b < 0) manfiy++;
+
+// // c ni 
+// if (c > 0) musbat++;
+// else if (c < 0) manfiy++;
+
+// console.log("Musbat sonlar:", musbat);
+// console.log("Manfiy sonlar:", manfiy);
