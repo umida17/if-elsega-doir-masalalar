a752757a935a87758565b847f639cb7963f57049
uy <a.normurzaev20@gmail.com>
2026-03-27 22:58:29 +0500

    1 masala yakunlandi

--- if else.js
+++ if else.js
@@ -1,4 +1,13 @@
 // if elsega doil masalalar
 
 
-let a = prompt("a sonini kiriting")
+// let a = +prompt("a sonini kiriting")
+
+// if (a > 0) {
+//     a = a + 1;
+// }else{
+//     a = a - 2;
+// }
+// console.log(a);
+
+
