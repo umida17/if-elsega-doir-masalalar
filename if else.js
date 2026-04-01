f5b50bcfbbab9faa4678b4eed0db8ea8bb724f97
uy <a.normurzaev20@gmail.com>
2026-04-01 23:28:58 +0500

    case.1 masala islandi

--- if else.js
+++ if else.js
@@ -473,24 +473,27 @@
 
 
 
-let k =+prompt("Bahoni kiriting (1-5):");
+// let k =+prompt("Bahoni kiriting (1-5):");
 
-switch (k) {
-    case 1:
-        console.log("yomon");
-        break;
-    case 2:
-        console.log("qoniqarsiz");
-        break;
-    case 3:
-        console.log("qoniqarli");
-        break;
-    case 4:
-        console.log("yaxshi");
-        break;
-    case 5:
-        console.log("a'lo");
-        break;
-    default:
-        console.log("xato");
-}
+// switch (k) {
+//     case 1:
+//         console.log("yomon");
+//         break;
+//     case 2:
+//         console.log("qoniqarsiz");
+//         break;
+//     case 3:
+//         console.log("qoniqarli");
+//         break;
+//     case 4:
+//         console.log("yaxshi");
+//         break;
+//     case 5:
+//         console.log("a'lo");
+//         break;
+//     default:
+//         console.log("xato");
+// }
+
+
+
