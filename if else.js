6a5101f259d4f69ac22217a438a59902df3a48cd
uy <a.normurzaev20@gmail.com>
2026-04-01 23:27:37 +0500

    case.1 masala

--- if else.js
+++ if else.js
@@ -473,3 +473,24 @@
 
 
 
+let k = parseInt(prompt("Bahoni kiriting (1-5):"));
+
+switch (k) {
+    case 1:
+        console.log("yomon");
+        break;
+    case 2:
+        console.log("qoniqarsiz");
+        break;
+    case 3:
+        console.log("qoniqarli");
+        break;
+    case 4:
+        console.log("yaxshi");
+        break;
+    case 5:
+        console.log("a'lo");
+        break;
+    default:
+        console.log("xato");
+}
