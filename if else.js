3b052c168e52d151e786b6df78a8ed2cdf112c0e
uy <a.normurzaev20@gmail.com>
2026-04-01 23:18:08 +0500

    17 masala

--- if else.js
+++ if else.js
@@ -431,3 +431,10 @@
 
 
 
+let yil = parseInt(prompt("Yilni kiriting:"));
+
+if ((yil % 4 === 0 && yil % 100 !== 0) || (yil % 400 === 0)) {
+    console.log("366 kun (kabisa yil)");
+} else {
+    console.log("365 kun");
+}
