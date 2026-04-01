c09db2cfca2133f77ad3c78407ca2cdd54a35b83
uy <a.normurzaev20@gmail.com>
2026-04-01 23:08:24 +0500

    14 masala ishlandi

--- if else.js
+++ if else.js
@@ -377,5 +377,22 @@
 
 
 
+let A = parseFloat(prompt("A ni kiriting:"));
+let B = parseFloat(prompt("B ni kiriting:"));
+let C = parseFloat(prompt("C ni kiriting:"));
 
+if ((A <= B && B <= C) || (A >= B && B >= C)) {
+     A = A * 2;
+    B = B * 2;
+    C = C * 2;
+} else {
+    // aks holda
+    A = -A;
+    B = -B;
+    C = -C;
+}
+
+console.log("A =", A);
+console.log("B =", B);
+console.log("C =", C);
 
